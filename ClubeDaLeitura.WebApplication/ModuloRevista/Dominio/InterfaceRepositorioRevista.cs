cd3d4f7e2a63da09115b294d222e2bf5d96fc76e
DrElucidator <dr.elucidator@gmail.com>
2026-05-26 17:18:58 -0300

    Implementada Interface Repositório de Revistas

---
+++ ClubeDaLeitura.WebApplication/ModuloRevista/Dominio/InterfaceRepositorioRevista.cs
@@ -0,0 +1,5 @@
+using ClubeDaLeitura.WebApplication.Compartilhado.Infraestrutura;
+
+namespace ClubeDaLeitura.WebApplication.ModuloRevista.Dominio;
+
+public interface InterfaceRepositorioRevista : InterfaceRepositorio<Revista>;
--- ClubeDaLeitura.WebApplication/ModuloRevista/Infra/RepositorioRevista.cs
+++ ClubeDaLeitura.WebApplication/ModuloRevista/Infra/RepositorioRevista.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace ClubeDaLeitura.WebApplication.ModuloRevista.Infraestrutura;
 
-public sealed class RepositorioRevista : RepositorioBase<Revista>
+public sealed class RepositorioRevista : RepositorioBase<Revista>, InterfaceRepositorioRevista
 {
     public RepositorioRevista(Serializable serializable) : base(serializable) { }
 
